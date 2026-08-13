ec7297ac45d4772c57c9f23834a45e9cde60f901
Reina <reinasosa2024@gmail.com>
2026-08-13 11:26:02 -0600

    v1 of the navbar

--- js/service/firebase.js
+++ js/service/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/12
 
 // Configuración
 const firebaseConfig = {
-  apiKey: "AIzaSyDmYYGRmUDEOtgbiO37l0hNfbZ0shC3cB0",
-  authDomain: "gestionrestaurante-99b84.firebaseapp.com",
-  projectId: "gestionrestaurante-99b84",
-  storageBucket: "gestionrestaurante-99b84.firebasestorage.app",
-  messagingSenderId: "436759352562",
-  appId: "1:436759352562:web:5fe3e2fe0631ad38f1ac6b",
-  measurementId: "G-E5D6H214L9"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
 };
 
 // Inicialización de Firebase
