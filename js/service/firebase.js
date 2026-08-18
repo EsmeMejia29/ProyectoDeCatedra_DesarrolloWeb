f2a5dc17e6d5d96ae0a643348e6933d4bc802b76
Reina <reinasosa2024@gmail.com>
2026-08-18 11:16:12 -0600

    v1 of about us page

--- js/service/firebase.js
+++ js/service/firebase.js
@@ -2,21 +2,18 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebas
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 
-// Configuración
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId
+  apiKey: "AIzaSyDmYYGRmUDEOtgbiO37l0hNfbZ0shC3cB0",
+  authDomain: "gestionrestaurante-99b84.firebaseapp.com",
+  projectId: "gestionrestaurante-99b84",
+  storageBucket: "gestionrestaurante-99b84.firebasestorage.app",
+  messagingSenderId: "436759352562",
+  appId: "1:436759352562:web:5fe3e2fe0631ad38f1ac6b",
+  measurementId: "G-E5D6H214L9"
 };
 
-// Inicialización de Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// Exportaciones
 export { auth, db, signInWithEmailAndPassword, doc, getDoc };
